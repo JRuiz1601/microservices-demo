4133410a38b6f6716b99c3df9ad97aceb2d10add
Tomás <tomasquin2003@gmail.com>
2026-04-10 23:27:19 -0500

    infra: bootstrap terraform main for aks and remote state

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg3"
+    storage_account_name = "tfstatemicrosvcdemo"
+    container_name       = "tfstate"
+    key                  = "microservices.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "222b144e-3ecd-41e6-a3b4-b30269d7ec31"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "${var.cluster_name}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
